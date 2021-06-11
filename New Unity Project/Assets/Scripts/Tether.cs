c0730ae5bc65424358d701a3a22313fe29940694
Deizama <The_way_of_ferret@hotmail.fr>
2021-06-11 23:55:54 +0200

    Modification of the unity version
    Changed the Unity Version of the project to 2020.3.11 + added some template + tether working.

---
+++ New Unity Project/Assets/Scripts/Tether.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tether : MonoBehaviour
+{
+    public GameObject humanCharacter;
+    public GameObject ghostCharacter;
+
+    public LineRenderer tether;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (humanCharacter != null && ghostCharacter != null)
+        {
+            tether.SetPosition(0, humanCharacter.transform.position);
+            tether.SetPosition(1, ghostCharacter.transform.position);
+        }
+    }
+}
